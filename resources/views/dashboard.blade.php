c47b2e879587912d1e209d61d3a3027610041664
eliascassal <eliascassal@gmail.com>
2021-09-10 11:43:44 -0400

    implementado tailwind

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,13 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+               <p class="container"> <a href="/user"> Usuarios </a></p>
+               <p class="container"> <a href="/user/create"> Nuevo Usuario</a></p>
+               <p class="container"> <a href="/deudor/"> Deudores </a></p>
+               <p class="container"> <a href="/prestamos"> prestamos vigentes </a></p>
+
+
+                
             </div>
         </div>
     </div>
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,27 +1,25 @@
 @extends('layouts.layout')
 
-@section('title', 'Cursos')
+@section('title', 'Usuarios')
 
 @section('content')
 
 
 <h1>Bienvenido a la seccion de los usuarios </h1>
-
-<div class="row">
+<div class=" max-w-md  max-width: 28rem/* 448px */ bg-gray-300 mx-auto">
+<div class="grid sm-grid-cols-1 md-grid-cols-2 lg-grid-cols-4 gap-4">
     @foreach($user as $user)
-        <div class="col-sm">
-            <div class="card text-center" Style= "width: 18rem; margin-top:50px;">
-                    <div class="card-body ">
-                    <h5 class="card-title">{{$user->name}}</h5>
-                    <p class="card-text"> una descripcion de ejemplo para rellenar con un poco de codigo hardcore y que nos muestre como esta quedando la listilla aguante informatica </p>
-                    <a href="/user/{{$user->id}}" class="btn btn-primary">Ver mas...{{$user->id}}</a>
+        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <div class="md:flex">
+             <div class="p-8">
+                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{{$user->name}}</div>
+                    <p class="mt-2 text-gray-500">una descripcion de ejemplo para rellenar con un poco de codigo hardcore, aguante informatica </p>
+                    <a href="/user/{{$user->id}}" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Ver mas...</a>
                 </div>
             </div>
-        </div>
+         </div>
     @endforeach
 </div>
-
-
-
+</div>
 @endsection
 
